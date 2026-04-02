7a97f067aed8d277cb6054f103b6a35861d3f93b
MatthewWYellow <90801594+MatthewWYellow@users.noreply.github.com>
2026-04-02 15:03:25 -0700

    Add first time login setup for warrior attributes

--- UDPServer.py
+++ UDPServer.py
@@ -13,6 +13,7 @@ class Warrior:
     sheild: int = -1
     slayingPotion: int = -1
     healingPotion: int = -1
+    firstTimeLogin: bool = True
 
 users = {
     "A": Warrior("A", "A"),
@@ -21,7 +22,7 @@ users = {
     "D": Warrior("D", "D")
 }
 fightRequests = []
-#print ('The server is ready to receive')
+print ('The server is ready to receive')
 waitingForUsername = True
 while waitingForUsername:
     usernameInput, clientAddress = serverSocket.recvfrom(2048)
@@ -44,6 +45,54 @@ while waitingForPassword:
     else:
         loginResponse = "Password incorrect, try again"
         serverSocket.sendto(loginResponse.encode(), clientAddress)
+
+if warriorLoggingIn.firstTimeLogin:
+    serverSocket.sendto("First time login".encode(), clientAddress)
+while warriorLoggingIn.firstTimeLogin:
+    serverSocket.sendto("Input sword strength:".encode(), clientAddress)
+    swordSetup = False
+    while not swordSetup:
+        swordInput, clientAddress = serverSocket.recvfrom(2048)
+        if swordInput.decode().isdigit() and 0 <= int(swordInput.decode()) <= 3:
+            warriorLoggingIn.sword = int(swordInput.decode())
+            swordSetup = True
+        else:
+            serverSocket.sendto("Invalid sword strength. Please enter a number between 0 and 3:".encode(), clientAddress)
+    sheildSetup = False
+    while not sheildSetup:
+        serverSocket.sendto("Input sheild strength:".encode(), clientAddress)
+        sheildInput, clientAddress = serverSocket.recvfrom(2048)
+        if sheildInput.decode().isdigit() and 0 <= int(sheildInput.decode()) <= 3:
+            warriorLoggingIn.sheild = int(sheildInput.decode())
+            sheildSetup = True
+        else:
+            serverSocket.sendto("Invalid sheild strength. Please enter a number between 0 and 3:".encode(), clientAddress)
+    slayingPotionSetup = False
+    while not slayingPotionSetup:
+        serverSocket.sendto("Input slaying potion strength:".encode(), clientAddress)
+        slayingPotionInput, clientAddress = serverSocket.recvfrom(2048)
+        if slayingPotionInput.decode().isdigit() and 0 <= int(slayingPotionInput.decode()) <= 3:
+            warriorLoggingIn.slayingPotion = int(slayingPotionInput.decode())
+            slayingPotionSetup = True
+        else:
+            serverSocket.sendto("Invalid slaying potion strength. Please enter a number between 0 and 3:".encode(), clientAddress)
+    healingPotionSetup = False
+    while not healingPotionSetup:
+        serverSocket.sendto("Input healing potion strength:".encode(), clientAddress)
+        healingPotionInput, clientAddress = serverSocket.recvfrom(2048)
+        if healingPotionInput.decode().isdigit() and 0 <= int(healingPotionInput.decode()) <= 3:
+            warriorLoggingIn.healingPotion = int(healingPotionInput.decode())
+            healingPotionSetup = True
+        else:
+            serverSocket.sendto("Invalid healing potion strength. Please enter a number between 0 and 3:".encode(), clientAddress)
+    if warriorLoggingIn.sword + warriorLoggingIn.sheild + warriorLoggingIn.slayingPotion + warriorLoggingIn.healingPotion != 10:
+        serverSocket.sendto("Total strength is not 10. Please re-enter your strengths.".encode(), clientAddress)
+        continue
+    else:
+        warriorLoggingIn.firstTimeLogin = False
+        serverSocket.sendto("Total strength is 10. Setup complete.".encode(), clientAddress)
+        break
+
     #message, clientAddress = serverSocket.recvfrom(2048)
     #modifiedMessage = message.decode().upper()
     #serverSocket.sendto(modifiedMessage.encode(), clientAddress)
